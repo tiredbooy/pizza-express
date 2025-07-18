cf2cdf01e8722d8235ef48cfd1a3c934cd024a96
tiredbooy <mahdykazemyo1i2@gmail.com>
2025-07-18 15:31:50 +0330

    used navigation method from react router for loading state

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
+import Loader from "./Loader";
 
-function AppLayout () {
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div className="">
+    <div className="layout">
+      {isLoading && <Loader />}
+
       <Header />
 
       <main>
@@ -16,4 +22,4 @@ function AppLayout () {
   );
 }
 
-export default AppLayout; 
+export default AppLayout;
