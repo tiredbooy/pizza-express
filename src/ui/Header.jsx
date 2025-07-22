2b00499c0cf7a1551ce255d2c3aa3d640669ce16
tiredbooy <mahdykazemyo1i2@gmail.com>
2025-07-22 23:26:44 +0330

    updated text styles

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,17 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
-function Header () {
+function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">Fast React Pizza Co.</Link>
-      
-      <SearchOrder  />
+    <header className="uppercase bg-yellow-500">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
 
-      <p>Tiredboy</p>
+      <SearchOrder />
+
+      <Username />
     </header>
   );
 }
