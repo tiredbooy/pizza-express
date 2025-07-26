564025fab75763684fbe8d9211ba3414bb0629ca
tiredbooy <mahdykazemyo1i2@gmail.com>
2025-07-27 02:17:01 +0330

    styled some inputs

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable */
 
-import { useState } from 'react';
-import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
-import { createOrder } from '../../services/apiRestaurant';
+import { useState } from "react";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -13,21 +13,21 @@ const isValidPhone = (str) =>
 const fakeCart = [
   {
     pizzaId: 12,
-    name: 'Mediterranean',
+    name: "Mediterranean",
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: 'Vegetale',
+    name: "Vegetale",
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: 'Spinach and Mushroom',
+    name: "Spinach and Mushroom",
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -37,7 +37,7 @@ const fakeCart = [
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === 'submitting';
+  const isSubmitting = navigation.state === "submitting";
 
   const formErrors = useActionData();
 
@@ -65,12 +65,18 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input
+              className="w-full px-4 py-2 text-sm transition-all duration-300 border rounded-full md:py-3 border-stone-200 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6"
+              type="text"
+              name="address"
+              required
+            />
           </div>
         </div>
 
         <div>
           <input
+            className="w-6 h-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
@@ -86,7 +92,7 @@ function CreateOrder() {
             disabled={isSubmitting}
             className="px-4 py-3 font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
           >
-            {isSubmitting ? 'Placing order...' : 'Order now'}
+            {isSubmitting ? "Placing order..." : "Order now"}
           </button>
         </div>
       </Form>
@@ -100,13 +106,13 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === 'on',
+    priority: data.priority === "on",
   };
 
   const errors = {};
   if (!isValidPhone(order.phone))
     errors.phone =
-      'Please Give us your correct phone number. We might need it to contact you.';
+      "Please Give us your correct phone number. We might need it to contact you.";
 
   if (Object.keys(errors).length > 0) return errors;
 
--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,21 +1,26 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function SearchOrder () {
-    const [query,setQuery] = useState("");
-    const navigate = useNavigate();
+function SearchOrder() {
+  const [query, setQuery] = useState('');
+  const navigate = useNavigate();
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        if(!query) return;
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
 
-        navigate(`/order/${query}`);
-        setQuery("")
-    }
+    navigate(`/order/${query}`);
+    setQuery('');
+  }
 
   return (
     <form onSubmit={handleSubmit}>
-        <input placeholder="Search order #" value={query} onChange={(e) => setQuery(e.target.value)} />
+      <input
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="px-4 py-2 text-sm transition-all duration-300 bg-yellow-100 rounded-full w-28 placeholder:text-stone-400 sm:focus:w-72 sm:w-64 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50"
+      />
     </form>
   );
 }
