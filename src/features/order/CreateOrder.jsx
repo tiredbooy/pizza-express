87ecb0c0d825aec12628b1d11b653d8eff182032
tiredbooy <mahdykazemyo1i2@gmail.com>
2025-07-27 14:31:26 +0330

    completed loader style , some small changes in the other styles

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -51,13 +51,13 @@ function CreateOrder() {
       <Form method="POST" action="">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -66,7 +66,7 @@ function CreateOrder() {
           <label>Address</label>
           <div>
             <input
-              className="w-full px-4 py-2 text-sm transition-all duration-300 border rounded-full md:py-3 border-stone-200 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6"
+              className="input"
               type="text"
               name="address"
               required
--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Button from '../../ui/Button';
 
 function CreateUser() {
   const [username, setUsername] = useState('');
@@ -16,12 +17,12 @@ function CreateUser() {
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
-        className='px-4 py-2 bg-black w-72 rounded-xl'
+        className='mb-8 w-72 input'
       />
 
       {username !== '' && (
         <div>
-          <button>Start ordering</button>
+          <Button disabled={false}>Start ordering</Button>
         </div>
       )}
     </form>
