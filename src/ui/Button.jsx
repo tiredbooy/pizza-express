6861a011b8f88212c0cc9c062c0df3478587a2a8
tiredbooy <mahdykazemyo1i2@gmail.com>
2025-07-31 14:02:46 +0330

    appied some style in froms and some buttons

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,15 +2,14 @@
 import { Link } from "react-router-dom";
 
 function Button({ children, disabled, to, type }) {
-  const className =
-    "px-4 py-3 font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
-
-    const base = "font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
-   
+  const base =
+    "font-semibold text-sm tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
   const styles = {
-    primary : base + ' px-4 py-3 md:px-6 md:py-4',
-    small : base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs'
+    primary: base + " px-4 py-3 md:px-6 md:py-4",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      "font-semibold text-sm px-4 py-2.5 md:px-6 md:py-3.5 tracking-wide uppercase transition-colors duration-300 border-2 border-stone-300 rounded-full text-stone-400 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed",
   };
 
   if (to)
