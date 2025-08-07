e7d61ae62453ff99f14ec9df63f2595de39e0d75
tiredbooy <mahdykazemyo1i2@gmail.com>
2025-08-07 19:32:02 +0330

    removed browser router from main.js updated app.jsx browser router

--- src/App.jsx
+++ src/App.jsx
@@ -7,38 +7,36 @@ import Order, { loader as orderLoader } from "./features/order/Order";
 import CreateOrder, {
   action as createOrderAction,
 } from "./features/order/CreateOrder";
-import {action as UpdateOrderAction} from "./features/order/UpdateOrder"
+import { action as UpdateOrderAction } from "./features/order/UpdateOrder";
 import AppLayout from "./ui/AppLayout";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      element: <AppLayout />,
+      errorElement: <Error />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/menu", element: <Menu />, loader: menuLoader },
+        { path: "/cart", element: <Cart /> },
+        {
+          path: "/order/new",
+          element: <CreateOrder />,
+          action: createOrderAction,
+        },
+        {
+          path: "/order/:orderId",
+          element: <Order />,
+          loader: orderLoader,
+          action: UpdateOrderAction,
+        },
+      ],
+    },
+  ],
   {
-    element: <AppLayout />,
-    errorElement: <Error />,
-
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/menu",
-        element: <Menu />,
-        loader: menuLoader,
-        errorElement: <Error />,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-      {
-        path: "/order/new",
-        element: <CreateOrder />,
-        action: createOrderAction,
-      },
-      { path: "/order/:orderId", element: <Order />, loader: orderLoader , action : UpdateOrderAction },
-    ],
+    basename: "/pizza-express", // 👈 Add this line
   },
-]);
+);
 
 function App() {
   return <RouterProvider router={router} />;
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,11 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store.js";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/pizza-express">
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
   </React.StrictMode>,
 );
